18d31464a0cdc9ed67f6a4bc281e9cdd0b78a6d7
Olusesan <olusesan.ajina@gmail.com>
2016-10-28 21:48:47 -0700

    Initial Commit

---
+++ serverTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  serverTest2
+//
+//  Created by Olusesan Ajina on 10/28/16.
+//  Copyright © 2016 Sesan Ajina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
